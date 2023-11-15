1d5911fd9b39e1e6ac4d33947761a2df23287357
ahmednafea <ahmednafea-7@github.com>
2023-11-15 21:20:59 +0200

    first commit

---
+++ task.js
@@ -0,0 +1,85 @@
+
+// document.getElementsByClassId("red").addEventListener=("click",changecolor);
+// changecolor()
+// {
+//   html.style.setproperty('--main-color','red')
+// } 
+/* var r = document.querySelector(':root');
+r.style.setproperty('--main-color','red'); */
+// Array.from(document.querySelector(".BallsContainer").childern).forEach(e=>e.style.backGround = e.id)
+
+/**
+ * Swiper 11.0.4
+ * Most modern mobile touch slider and framework with hardware accelerated transitions
+ * https://swiperjs.com
+ *
+ * Copyright 2014-2023 Vladimir Kharlampidi
+ *
+ * Released under the MIT License
+ *
+ * Released on: November 9, 2023
+ */
+/**
+ * Swiper 11.0.4
+ * Most modern mobile touch slider and framework with hardware accelerated transitions
+ * https://swiperjs.com
+ *
+ * Copyright 2014-2023 Vladimir Kharlampidi
+ *
+ * Released under the MIT License
+ *
+ * Released on: November 9, 2023
+ */
+// import Swiper from 'swiper/bundle';
+
+// import styles bundle
+// import 'swiper/css/bundle';
+
+
+
+//  const swiper = new Swiper('.swiper', {
+//     Optional parameters,
+//     loop: true,
+//     If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+//     Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+  
+//     And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+//     breakpoints: {
+//       320:{
+//         slidesPerView:2,
+//         spaceBetween:20
+//       },
+//       480 :{
+//         slidesPerView:3,
+//         spaceBetween:30
+//       },
+//       640 :{
+//         slidesPerView:4,
+//         spaceBetween:40
+//       }
+//     }
+//   }); 
+
+  //  const samola = document.querySelector(".colorscont");
+  //  const btn = document.getElementById("icon");
+  //       btn.addEventListener("click", function () {
+  //         html.style.setproperty('--main-color','red')
+
+  //       } );
+   
+  //  colorred()
+  //  {
+  //    html.style.setproperty('--main-color','red')
+  //  } 
+
+ 
